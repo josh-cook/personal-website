393c58e06fff334b7ad4e9ec2fc53aa82059f9c2
joshua-cook <joshua.cook@onthebeach.co.uk>
2019-12-31 10:31:13 +0000

    Add text image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
 import HomePageImage from './components/HomepageImage';
+import JoshText from './components/JoshText';
 import Content from './components/Content';
+import GithubLinkImage from './components/GithubLinkImage';
 
 
 function App() {
   return (
     <div className="app">
-        <div className="logo">
-          <HomePageImage />
-        </div>
-        
         <header className="app-header">
-          <Content />
+          <div className="logo">
+            <HomePageImage />
+          </div>
+          <div className="github-logo">
+            <GithubLinkImage /> 
+          </div>
         </header>
+
+        <div className="content-area">
+          <p>
+            <JoshText />
+          </p>
+          <Content />
+        </div>
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,11 @@
+import  React from "react";
+
 function Content() {
-    return ( 
-        "Welcome to my website"
+    return (
+        <div>
+            A developer based in the heart of Manchester, desired to game like every other 'techie' there seems to be.
+            Whilst playing music with a couple of close friends.
+        </div>
     );
 }
 
